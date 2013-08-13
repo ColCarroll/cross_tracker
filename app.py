610ec35926e1f1f9943d1995015f4a078b5cba70
Colin Carroll <colcarroll@gmail.com>
2013-08-12 22:37:07 -0500

    Added templates, database

--- app.py
+++ app.py
@@ -1,28 +1,67 @@
+""" Controller for APP
+"""
 import os
-from flask import Flask
+import json
+from flask import Flask, render_template, send_from_directory
 
 #----------------------------------------
 # initialization
 #----------------------------------------
 
-app = Flask(__name__)
+APP = Flask(__name__)
+# Keep passwords and credentials in a .creds file
+CREDS = json.load(open(".creds"))
 
-app.config.update(
+APP.config.update(
     DEBUG = True,
 )
+
+APP.config["SECRET_KEY"] = "\xc62{{x.\xf6\xe1_K\xf3\x85)~\xb3E\xce)\x89j\x823|'"
+
+#----------------------------------------
+# database
+#----------------------------------------
+
+from mongoengine import connect
+from flask.ext.mongoengine import MongoEngine
+
+APP.config["MONGODB_DB"] = CREDS['DB_NAME']
+connect(CREDS['DB_NAME'],
+    host='mongodb://' +
+    CREDS['DB_USERNAME'] +
+    ':' +
+    CREDS['DB_PASSWORD'] +
+    '@' +
+    CREDS['DB_HOST_ADDRESS'])
+DB = MongoEngine(APP)
 
 #----------------------------------------
 # controllers
 #----------------------------------------
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+@APP.route('/favicon.ico')
+def favicon():
+  """Route favicon to all pages
+  """
+  return send_from_directory(
+      os.path.join(APP.root_path, 'static'), 'ico/favicon.ico')
+
+@APP.errorhandler(404)
+def page_not_found(_):
+  """404 page
+  """
+  return render_template('404.html'), 404
+
+@APP.route("/")
+def index():
+  """Front page for app
+  """
+  return render_template('index.html')
 
 #----------------------------------------
 # launch
 #----------------------------------------
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+  PORT = int(os.environ.get("PORT", 5000))
+  APP.run(host='0.0.0.0', port=PORT)
