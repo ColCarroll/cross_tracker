308013b305efb5e72393ede73d2c5b4ee05bf9d7
Colin Carroll <colcarroll@gmail.com>
2013-08-19 00:51:24 -0500

    Some field validation

--- static/js/customjs.js
+++ static/js/customjs.js
@@ -1,4 +1,5 @@
 jQuery(function($) {
+
     $('form[data-async]').on('submit', function(event) {
         var $form = $(this);
         var $target = $($form.attr('data-target'));
@@ -15,4 +16,21 @@ jQuery(function($) {
  
         event.preventDefault();
     });
+});
+
+$(document).ready(function () {
+  $(".resultfields").find(".resultfield").each(function () {
+    var elem = $(this);
+    elem.bind("propertychange keyup input paste", function (event) {
+      var otherfields = elem.closest(".resultfields").find(".resultfield").not($(this));
+      var button = elem.closest("form").find(":submit");
+      if (elem.val().length) {
+        otherfields.attr("disabled", "disabled");
+        button.removeAttr("disabled");
+      } else {
+        otherfields.removeAttr("disabled");
+        button.prop("disabled",true);
+      }
+    });
+  });
 });
